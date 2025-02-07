58a0f8bd386311b2e5da8ce2056c35688fce2533
gbenga <odetoyinbog@gmail.com>
2025-02-07 16:13:08 +0100

    7-10

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const btn = document.querySelector(".btn")
 const input = document.querySelector(".input")
 
 
+btn.addEventListener('click', ()=> {
+  search.classList.toggle('active')
+  input.focus()
+})
